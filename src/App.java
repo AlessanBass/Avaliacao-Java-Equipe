ccde63d9b31dd9d1a46f6949b8359a5e47380651
IanRobertLuzPinheiro <149024071+IanRobertLuzPinheiro@users.noreply.github.com>
2023-12-07 11:54:50 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class App {
-    //Nessa classe que vai ficar toda a lógica do negocio
-    // Ela contém o método Main() onde o programa começa a ser executado
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         int op;
@@ -34,7 +32,7 @@ public class App {
                 break;
 
                 case 5:
-                    System.out.println("Em construcao");
+                    menuGestaoFalhas(scanner, sistema);
                 break;
 
                 case 0:
@@ -48,3 +46,86 @@ public class App {
         }while(op != 0);
     }
 }
+
+private void menuGestaoFalhas(Scanner scanner, Sistema sistema) {
+    int op;
+    do {
+        exibirMenuGestaoFalhasOpcoes();
+        System.out.print("Escolha uma opção para Gestão de Falhas: ");
+        op = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (op) {
+            case 1:
+                adicionarFalha(scanner, sistema);
+                break;
+            case 2:
+                exibirFalhas(sistema);
+                break;
+            case 3:
+                menuGestaoFalhasDistribuicao(scanner, sistema);
+                break;
+            case 4:
+                menuGestaoFalhasGeracao(scanner, sistema);
+                break;
+            case 0:
+                System.out.println("Retornando ao menu principal.");
+                break;
+            default:
+                System.out.println("Ops... Opcao invalida!");
+        }
+
+    } while (op != 0);
+}
+
+private void menuGestaoFalhasDistribuicao(Scanner scanner, Sistema sistema) {
+    int op;
+    do {
+        exibirMenuGestaoFalhasDistribuicaoOpcoes();
+        System.out.print("Escolha uma opção para Gestão de Falhas de Distribuição: ");
+        op = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (op) {
+            case 1:
+                adicionarFalhaDistribuicao(scanner, sistema);
+                break;
+            case 2:
+                listarReparosFalhaDistribuicao(scanner, sistema);
+                break;
+            case 3:
+                encerrarReparoFalhaDistribuicao(scanner, sistema);
+                break;
+            case 0:
+                System.out.println("Retornando ao menu de Gestão de Falhas.");
+                break;
+            default:
+                System.out.println("Opcao invalida!");
+        }
+
+    } while (op != 0);
+}
+    
+    private void menuGestaoFalhasGeracao(Scanner scanner, Sistema sistema) {
+    
+    }
+    
+    private void adicionarFalhaDistribuicao(Scanner scanner, Sistema sistema) {
+    
+    }
+    
+    private void listarReparosFalhaDistribuicao(Scanner scanner, Sistema sistema) {
+    
+    }
+    
+    private void encerrarReparoFalhaDistribuicao(Scanner scanner, Sistema sistema) {
+    
+    }
+    
+    private void exibirMenuGestaoFalhasDistribuicaoOpcoes() {
+        System.out.println("1. Adicionar Falha de Distribuição");
+        System.out.println("2. Listar Reparos de Falha de Distribuição");
+        System.out.println("3. Encerrar Reparo de Falha de Distribuição");
+        System.out.println("0. Voltar ao Menu de Gestão de Falhas");
+    }
+
